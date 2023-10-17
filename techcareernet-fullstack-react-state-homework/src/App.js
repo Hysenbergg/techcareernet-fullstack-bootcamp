73073313055bba510df5d7ba2b77676c0282fda6
Hysenbergg <huseyinsaidzeyrek@gmail.com>
2023-10-17 11:41:37 +0300

    React State Homework is completed

---
+++ techcareernet-fullstack-react-state-homework/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import WidthHeightCalc from './components/WidthHeightCalc';
+
+function App() {
+  return (
+    <div>
+      <WidthHeightCalc />
+    </div>
+  );
+}
+
+export default App;
---
+++ techcareernet-fullstack-react-state-homework/src/components/WidthHeightCalc.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+
+function WidthHeightCalc() {
+  const [width, setWidth] = useState(100);
+  const [height, setHeight] = useState(100);
+
+  const changeSize = () => {
+    // Yeni boyutları almak için state değerlerini kullan
+    const newWidth = parseInt(document.getElementById("widthInput").value);
+    const newHeight = parseInt(document.getElementById("heightInput").value);
+
+    // Yeni boyutları ayarla
+    if (!isNaN(newWidth) && !isNaN(newHeight)) {
+      setWidth(newWidth);
+      setHeight(newHeight);
+    }
+  };
+
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            marginTop: "30px",
+            marginBottom: "30px",
+          }}
+        >
+          <input id="widthInput" type="number" placeholder="Width" />
+          <input id="heightInput" type="number" placeholder="Height" />
+        </div>
+        <div style={{
+            marginBottom: "30px",
+        }}>
+          <button onClick={changeSize}>Calc</button>
+        </div>
+        <div
+          style={{
+            width: width + "px",
+            height: height + "px",
+            backgroundColor: "lightblue",
+            border: "5px solid red",
+          }}
+        >
+          Bu divin boyutu {width}x{height} piksel.
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default WidthHeightCalc;
